4fff24070c6c47664ea5d75d234fde66372e2137
Julian Jackson <julian.jackson004@gmail.com>
2020-12-14 18:08:19 +1000

    Added results page template

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import React, { Component } from "react";
 
 import Main from "./components/Main";
+import Results from "./components/Results"
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route path="/" component={Main} exact />
+          <Route path="/results" component={Results} />
         </Switch>
       </BrowserRouter>
     );
